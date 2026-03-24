254cb0bcd8cc9ba5105b0028d2397cd23a52611d
larajc123 <laracollinge@outlook.com>
2026-03-24 19:14:42 +0000

    Hashing passwords and connecting vendor login to database using mySQL

--- python/vendorLogin.py
+++ python/vendorLogin.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+import mySQL.connector
 
 app = FastAPI()
 
@@ -18,15 +19,41 @@ vendors = [
     {"email" : "unionbrew@liverpool.ac.uk", "password": "student2026", "name": "Union Brew"},
 ]
 
+def getDatabase():
+    dataBase = mySQL.connector.connect (
+        host = "localhost",
+        user = "user",
+        password = "password"
+    )
+
+
 class loginRequest(BaseModel):  #  this does automatic data conversions and validaton checks 
     email: str
     password: str
 
 @app.post("/login")
 def login(data: loginRequest):
+
+    database = getDatabase()
+    cursorObject = database.cursor()
+
+    cursorObject.execute("SELECT name, password_hash")# %s is a placeholder to prevent SQL injection, comma is needed after (data.email,) so it is treated as a tuple
+
+    vendors = cursorObject.fetchall()
+
     for vendor in vendors:
         if vendor["email"] == data.email and vendor["password"] == data.password:
             return {"message": "login successful", "name": vendor["name"]} 
         
     return {"error": "incorrect email or password"} 
 
+
+# hashing passwords 
+
+import hashlib
+
+password1 = "jacketPotatoes"
+salt =  "5ab"
+spudPass = password1 + salt
+spudPassHashed = password1.sha256(spudPass.encode())
+print(spudPassHashed)
