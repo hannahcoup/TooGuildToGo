1674564a9f02358669b4f1ae5e52e85aaad659c8
bliseno <hussein.shaverdi@gmail.com>
2026-04-03 18:46:14 +0100

    Update 03_indexes.sql

--- database/03_indexes.sql
+++ database/03_indexes.sql
@@ -1,25 +1,21 @@
-CREATE INDEX idx_food_category              ON food(category);
-CREATE INDEX idx_food_allergen_food         ON food_allergen(food_id);
-CREATE INDEX idx_food_allergen_allergen     ON food_allergen(allergen_id);
-
-CREATE INDEX idx_vendor_food_items_vendor   ON vendor_food_items(vendor_id);
-CREATE INDEX idx_vendor_food_items_food     ON vendor_food_items(food_id);
-
-CREATE INDEX idx_bags_vendor                ON bags(vendor_id);
-CREATE INDEX idx_bags_status                ON bags(status);
-CREATE INDEX idx_bags_pickup_start          ON bags(pickup_window_start);
-CREATE INDEX idx_bags_pickup_end            ON bags(pickup_window_end);
-CREATE INDEX idx_bags_expires_at            ON bags(expires_at);
-CREATE INDEX idx_bags_product_name          ON bags(product_name);
-
-CREATE INDEX idx_bag_items_food             ON bag_items(food_id);
-CREATE INDEX idx_bag_dtags_tag              ON bag_dietary_tags(dietary_tag_id);
-
-CREATE INDEX idx_user_preferences_tag       ON user_dietary_preferences(dietary_tag_id);
-CREATE INDEX idx_user_exclusions_allergen   ON user_allergen_exclusions(allergen_id);
-
-CREATE INDEX idx_reservations_user          ON reservations(user_id);
-CREATE INDEX idx_reservations_bag           ON reservations(bag_id);
-CREATE INDEX idx_reservations_status        ON reservations(status);
-CREATE INDEX idx_reservations_payment       ON reservations(payment_status);
-
+CREATE INDEX idx_food_category ON food(category);
+CREATE INDEX idx_food_allergen_food ON food_allergen(food_id);
+CREATE INDEX idx_food_allergen_allergen ON food_allergen(allergen_id);
+CREATE INDEX idx_vendor_food_items_vendor ON vendor_food_items(vendor_id);
+CREATE INDEX idx_vendor_food_items_food ON vendor_food_items(food_id);
+CREATE INDEX idx_bags_vendor ON bags(vendor_id);
+CREATE INDEX idx_bags_status ON bags(status);
+CREATE INDEX idx_bags_pickup_start ON bags(pickup_window_start);
+CREATE INDEX idx_bags_pickup_end ON bags(pickup_window_end);
+CREATE INDEX idx_bags_expires_at ON bags(expires_at);
+CREATE INDEX idx_bags_product_name ON bags(product_name);
+CREATE INDEX idx_bag_items_food ON bag_items(food_id);
+CREATE INDEX idx_bag_dtags_tag ON bag_dietary_tags(dietary_tag_id);
+CREATE INDEX idx_user_preferences_tag ON user_dietary_preferences(dietary_tag_id);
+CREATE INDEX idx_user_exclusions_allergen ON user_allergen_exclusions(allergen_id);
+CREATE INDEX idx_reservations_user ON reservations(user_id);
+CREATE INDEX idx_reservations_bag ON reservations(bag_id);
+CREATE INDEX idx_reservations_status ON reservations(status);
+CREATE INDEX idx_reservations_payment ON reservations(payment_status);
+CREATE INDEX idx_cart_items_user ON cart_items(user_id);
+CREATE INDEX idx_cart_items_bag ON cart_items(bag_id);
