0d9f371b831e907f127d887cb8ac1c7cdbe786c8
bliseno <hussein.shaverdi@gmail.com>
2026-04-19 17:03:10 +0100

    Update main.py to fix CORS

--- python/main.py
+++ python/main.py
@@ -17,12 +17,15 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
+    '''
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "https://hannahcoup.github.io",
         "https://tooguildtogo-1.onrender.com",
     ],
+    '''
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
