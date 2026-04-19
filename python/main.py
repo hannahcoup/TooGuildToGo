47ba1ef1a31320f73c93163a43e4d7b983977c96
bliseno <hussein.shaverdi@gmail.com>
2026-04-19 22:56:50 +0100

    Update main.py

--- python/main.py
+++ python/main.py
@@ -18,7 +18,12 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
   
-    allow_origins=["*"],
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "https://tooguildtogo-1.onrender.com",
+        "https://tooguildtogo.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
